52233204c6f789565025058bcb497db9f4e85e0b
mrcc87 <mrcc87@gmail.com>
2015-06-02 13:34:12 +0200

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] # calls this method to check if the user is logged in before performing the actions
+  before_action :correct_user, only: [:edit, :update] # calls this method to check if the user is the correct one before performing the actions
+  before_action :admin_user, only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
@@ -19,10 +25,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "User has been successfully deleted."
+    redirect_to users_url
+  end
+
+  # checks to see if the user is logged in before performing an action. if not the user is redirected to the root url
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # confirms an admin user
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  # checks to see if the logged in user is the one the action is supposed to handle. if not, the user is redirected to the root url
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  private :user_params
+  private :user_params, :logged_in_user, :correct_user, :admin_user
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # calls the user remember method, puts the user_id and remember_token in a permanent cookie
   def remember user
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # sets the current_user variable to the user in the session. if the user was rememberd it logs the user in
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -22,19 +24,39 @@ module SessionsHelper
     end
   end
 
+  # checks to see if the passed user the the one currentlly logged in
+  def current_user? user
+    user == current_user
+  end
+
+  # checks to see if the user is logged in
   def logged_in?
     !current_user.nil?
   end
 
+  # delets the user remember_digest, the cookie user_id and cookie remember_token
   def forget user
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
+  # calls the forget method, deletes the user_id from the session and sets the current_user variable to nil
   def log_out
     forget current_user
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # redirects to stored location (or default)
+  def redirect_back_or default
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # stores the url trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
